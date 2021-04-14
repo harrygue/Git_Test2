48c75b344affede239fd0db8d9d35c83266e32ac
harrygue <hg070817.gue@gmail.com>
2021-04-14 16:21:43 +0200

    add file.js and h3 in index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 window.document.getElementsByTagName('h1')[0].innerHTML = 'Hello World'
 window.document.getElementsByTagName('h2')[0].innerHTML = 'Hello Harry'
+window.document.getElementsByTagName('h3')[0].innerHTML = 'Helle there'
